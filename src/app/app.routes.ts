7202608a1af449617c9714dc338a2ce88173a2de
rahul04070407 <rahulp@vareli.co.in>
2025-05-20 14:28:58 +0530

    comming soon page design

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutUsComponent } from './pages/about-us/about-us.component';
 import { ContactUsComponent } from './pages/contact-us/contact-us.component';
+import { ComingSoonComponent } from './pages/coming-soon/coming-soon.component';
 
 
 export const routes: Routes = [
@@ -15,11 +16,17 @@ export const routes: Routes = [
   },
   {
     path: 'about',
-    component: AboutUsComponent,
+    component: ComingSoonComponent,
   },
   {
     path: 'contact',
-    component: ContactUsComponent,
+    component: ComingSoonComponent,
   },
+    {
+    path: 'services',
+    component: ComingSoonComponent,
+  },
+  { path: 'coming-soon', component: ComingSoonComponent }
+
 ];
 
---
+++ src/app/pages/coming-soon/coming-soon.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-coming-soon',
+  imports: [],
+  standalone: true,
+  templateUrl: './coming-soon.component.html',
+  styleUrl: './coming-soon.component.scss'
+})
+export class ComingSoonComponent {
+
+}
